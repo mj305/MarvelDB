a505d04ff308f75ca9d6436ad899bc3ec10d54a7
mj305 <maria.j.beckles@gmail.com>
2020-06-18 10:12:48 -0400

    set up users.js models

--- index.js
+++ index.js
@@ -1,32 +1,71 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import userSchema from './models/users';
 
 const app = express()
 const port = 4000
 const users = [];
 
+mongoose.connect('mongodb+srv://mariab:monotono23@cluster0-mcj2c.mongodb.net/marveldb?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we're connected!")
+});
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.post('/signUp', (req,res) => {
-  const { name, email, password } = req.body
+app.post('/signUp', async (req,res) => {
 
-  users.push({
-    name,
-    email,
-    password,
-  })
+  try {
+    const { name, email, password } = req.body
 
-  res.json({
-    message: "Success... You're all Set"
-  })
+    const newUser = new userSchema({
+      name, 
+      email,
+      password,
+    })
+
+  const result = await newUser.save()
+
+  if(result ){
+    return  res.json({ message: "Success... You're all Set" })
+  } else {
+    return res.json({
+      message: "Ups"
+    })
+  }
+  } catch (error) {
+    console.log(error)
+    return res.json({
+      message: error,
+    })
+  }
+
+  
 })
 
-app.get('/users', (req, res) => {
+app.post('/signup', (req, res) => {
   res.json(users)
 })
 
+app.get('/users', async (req, res) => {
+  try {
+    const allUsers = await userSchema.find({})
+    return res.json(allUsers)
+  } catch (error) {
+    return res.json({
+      message: error,
+    })
+  }
+  
+})
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
 
 
 /* SIGN UP / LOGIN */
